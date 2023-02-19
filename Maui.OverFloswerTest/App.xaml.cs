d57d7379eb428cbc89010b51cc4e865f83f7891e
강준 <keisoft.kjun@gmail.com>
2023-02-19 23:38:15 +0900

    초기 커밋.

---
+++ Maui.OverFloswerTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.OverFloswerTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Maui.OverFloswerTest/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.OverFloswerTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
